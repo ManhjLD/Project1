66a790f96d52a3280f4157d59a2a71ed86ae9020
LyO <103374303+LLyOO@users.noreply.github.com>
2025-11-19 14:30:35 +0700

    update Model/Assignment.java

--- src/Model/Assignment.java
+++ src/Model/Assignment.java
@@ -1,4 +1,24 @@
-package Model;
+package model;
+import java.io.Serializable;
 
-public class Assignment {
+public class Assignment implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Player player;
+    private Position position;
+    private int matches;
+
+    public Assignment(Player player, Position position, int matches) {
+        this.player = player;
+        this.position = position;
+        this.matches = matches;
+    }
+
+    public Player getPlayer() { return player; }
+    public Position getPosition() { return position; }
+    public int getMatches() { return matches; }
+
+    @Override
+    public String toString() {
+        return player.getName() + " - " + position.getName() + " - matches=" + matches;
+    }
 }
